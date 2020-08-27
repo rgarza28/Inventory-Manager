331f049f5f0eab64eb00fe205ff5fdfa04a8bf3f
Robert Garza <garza.robert28@gmail.com>
2020-08-26 22:57:59 -0500

    redoing packages

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom"; //don't need to specify localhost url in axios http address
 
-//style
-import "spectre.css/dist/spectre.min.css";
-import "spectre.css/dist/spectre-icons.css";
-import "./index.css";
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
