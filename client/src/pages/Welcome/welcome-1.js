e85cdef0283984a07adbd86b60aa3577ebb4374d
vjeuel <vjeuel@gmail.com>
2020-08-26 20:21:55 -0500

    fixing binary welcome.js

---
+++ client/src/pages/Welcome/welcome-1.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../../components/NavBar/navbar";
+import Features from "./Features/Features";
+import Jumbotron from "./Jumbotron/Jumbotron";
+import Industries from "./Industries/Industries";
+import Customers from "./Customers/Customers";
+import Pricing from "./Pricing/Pricing";
+import Team from "./Team/Team";
+import Footer from "./Footer/Footer";
+import Tedtalk from "./Tedtalk/Tedtalk";
+
+function Welcome() {
+  return (
+    <header>
+      <Navbar />
+      <Jumbotron />
+      <Features />
+      <Tedtalk />
+      <Industries />
+      <Customers />
+      <Pricing />
+      <Team />
+      <Footer />
+    </header>
+  );
+}
+
+export default Welcome;
