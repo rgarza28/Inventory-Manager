035f7e1b77c1ac047de4c5cd3b810d5d492f1f85
Robert Garza <garza.robert28@gmail.com>
2020-09-12 07:38:52 -0500

    fixed case issue

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,74 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcypt = require('bcryptjs');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const auth = require('../../middleware/auth');
+const bcypt = require("bcryptjs");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
-const User = require('../../models/User');
+const User = require("../../models/user");
 
-router.post('/', (req, res) => {
-   const { email, password } = req.body;
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
 
-   //Validation
-   if (!email || !password) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
-   }
+  //Validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-   // check existing user
-   User.findOne({ email }).then((user) => {
-      if (!user) return res.status(400).json({ msg: 'User Does not exist' });
+  // check existing user
+  User.findOne({ email }).then((user) => {
+    if (!user) return res.status(400).json({ msg: "User Does not exist" });
 
-      // Validate password
-      bcypt.compare(password, user.password).then((isMatch) => {
-         if (!isMatch)
-            return res.status(400).json({ msg: 'Invalid credentials' });
+    // Validate password
+    bcypt.compare(password, user.password).then((isMatch) => {
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-         jwt.sign(
-            { id: user.id },
-            process.env.MONGODB_URI,
-            { expiresIn: 3600 },
-            (err, token) => {
-               if (err) throw err;
-               res.json({
-                  token,
-                  user: {
-                     id: user.id,
-                     name: user.name,
-                     email: user.email,
-                  },
-               });
-            }
-         );
-      });
-   });
+      jwt.sign(
+        { id: user.id },
+        process.env.MONGODB_URI,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            },
+          });
+        }
+      );
+    });
+  });
 });
 
-router.get('/user', auth, (req, res) => {
-   User.findById(req.user.id)
-      .select('-password')
-      .then((user) => res.json(user));
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then((user) => res.json(user));
 });
 
 router.post("/tokenIsValid", async (req, res) => {
-   try {
-     const token = req.header("x-auth-token");
-     if (!token) {
-     return res.json(false);
-     } 
-     const verified = jwt.verify(token, process.env.MONGODB_URI);
-     if (!verified) {
-     return res.json(false);
-     }
-     const user = await User.findById(verified.id);
-     if (!user) {
-     return res.json(false);
-     } else {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.json(false);
+    }
+    const verified = jwt.verify(token, process.env.MONGODB_URI);
+    if (!verified) {
+      return res.json(false);
+    }
+    const user = await User.findById(verified.id);
+    if (!user) {
+      return res.json(false);
+    } else {
       return res.json(true);
-     }
-   } catch (err) {
-     res.status(500).json({ error: err.message });
-   }
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,55 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcypt = require('bcryptjs');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const bcypt = require("bcryptjs");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
-const User = require('../../models/User');
+const User = require("../../models/user");
 
-router.post('/', (req, res) => {
-   const { name, email, password } = req.body;
+router.post("/", (req, res) => {
+  const { name, email, password } = req.body;
 
-   //Validation
-   if (!name || !email || !password) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
-   }
+  //Validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-   // check existing user
-   User.findOne({ email }).then((user) => {
-      if (user) return res.status(400).json({ msg: 'User already exist' });
+  // check existing user
+  User.findOne({ email }).then((user) => {
+    if (user) return res.status(400).json({ msg: "User already exist" });
 
-      const newUser = new User({
-         name,
-         email,
-         password
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    // Create salt and hash
+    bcypt.genSalt(10, (err, salt) => {
+      bcypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then((user) => {
+          jwt.sign(
+            { id: user.id },
+            process.env.MONGODB_URI,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                },
+              });
+            }
+          );
+        });
       });
-
-      // Create salt and hash
-      bcypt.genSalt(10, (err, salt) => {
-         bcypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser.save().then((user) => {
-               jwt.sign(
-                  { id: user.id },
-                  process.env.MONGODB_URI,
-                  { expiresIn: 3600 },
-                  (err, token) => {
-                     if (err) throw err;
-                     res.json({
-                        token,
-                        user: {
-                           id: user.id,
-                           name: user.name,
-                           email: user.email
-                        },
-                     });
-                  }
-               );
-            });
-         });
-      });
-   });
+    });
+  });
 });
 
 module.exports = router;
