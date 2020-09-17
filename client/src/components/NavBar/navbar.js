1902bd2772162422e68d532617d3872fcca15923
vjeuel <vjeuel@gmail.com>
2020-09-16 20:40:22 -0500

    sytling styling styling

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -1,174 +1,161 @@
 import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-   AppBar,
-   Box,
-   Button,
-   Avatar,
-   Grid,
-   Container,
-   Link,
-} from '@material-ui/core';
+import { AppBar, Box, Button, Avatar, Grid, Container, Link } from '@material-ui/core';
 import logo from './logo-horizontal.png';
 import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 
 function Navbar() {
-   const { userData, setUserData } = useContext(UserContext);
-   const classes = useStyles();
+	const { userData, setUserData } = useContext(UserContext);
+	const classes = useStyles();
 
-   const history = useHistory();
-   const welcome = () => history.push('/');
-   const signin = () => history.push('/signin');
-   const signup = () => history.push('/signup');
-   const logout = () => {
-      setUserData({
-         token: undefined,
-         user: undefined,
-      });
-      localStorage.setItem('auth-token', '');
-   };
+	const history = useHistory();
+	const welcome = () => history.push('/');
+	const signin = () => history.push('/signin');
+	const signup = () => history.push('/signup');
+	const home = () => history.push('/home');
+	const logout = () => {
+		setUserData({
+			token: undefined,
+			user: undefined,
+		});
+		localStorage.setItem('auth-token', '');
+	};
 
-   return (
-      <Container maxWidth={false}>
-         <AppBar className={classes.appBarStyle}>
-            <Grid container alignItems="center" style={{ padding: '0 15px' }}>
-               <Grid item className={classes.gridStyle} xs={12} md={3}>
-                  <Button onClick={welcome}>
-                     <Link href="/">
-                        <Avatar className={classes.avatarStyle} src={logo} />
-                     </Link>
-                  </Button>
-               </Grid>
-               <Grid className={classes.menuStyle} item xs={12} md={9}>
-                  <Button className={classes.textButtonStyle} href="#features">
-                     features
-                  </Button>
-                  <Button
-                     className={classes.textButtonStyle}
-                     href="#industries"
-                  >
-                     industries
-                  </Button>
-                  <Button className={classes.textButtonStyle} href="#customers">
-                     customers
-                  </Button>
-                  <Button className={classes.textButtonStyle} href="#pricing">
-                     pricing
-                  </Button>
-                  <Button className={classes.textButtonStyle} href="#team">
-                     team
-                  </Button>
-                  <Box className={classes.boxStyle}>
-                     {userData.token ? (
-                        <Button
-                           className={classes.containedButtonStyle}
-                           variant="text"
-                           onClick={logout}
-                        >
-                           sign out
-                        </Button>
-                     ) : (
-                        <>
-                           <Button
-                              className={classes.textButtonStyle}
-                              variant="text"
-                              onClick={signin}
-                           >
-                              sign in
-                           </Button>
-                           <Button
-                              className={classes.containedButtonStyle}
-                              variant="text"
-                              onClick={signup}
-                           >
-                              get started
-                           </Button>
-                        </>
-                     )}
-                  </Box>
-               </Grid>
-            </Grid>
-         </AppBar>
-      </Container>
-   );
+	return (
+		<Container maxWidth={false}>
+			<AppBar className={classes.appBarStyle}>
+				<Grid container alignItems='center' style={{ padding: '0 15px' }}>
+					<Grid item className={classes.gridStyle} xs={12} md={3}>
+						<Button onClick={welcome}>
+							<Link href='/'>
+								<Avatar className={classes.avatarStyle} src={logo} />
+							</Link>
+						</Button>
+					</Grid>
+					<Grid className={classes.menuStyle} item xs={12} md={9}>
+						<Button className={classes.textButtonStyle} href='#features'>
+							features
+						</Button>
+						<Button className={classes.textButtonStyle} href='#industries'>
+							industries
+						</Button>
+						<Button className={classes.textButtonStyle} href='#customers'>
+							customers
+						</Button>
+						<Button className={classes.textButtonStyle} href='#pricing'>
+							pricing
+						</Button>
+						<Button className={classes.textButtonStyle} href='#team'>
+							team
+						</Button>
+						<Box className={classes.boxStyle}>
+							{userData.token ? (
+								<>
+									<Button
+										className={classes.containedButtonStyle}
+										variant='text'
+										onClick={home}
+										style={{ marginRight: '20px' }}>
+										home
+									</Button>
+									<Button className={classes.containedButtonStyle} variant='text' onClick={logout}>
+										sign out
+									</Button>
+								</>
+							) : (
+								<>
+									<Button className={classes.textButtonStyle} variant='text' onClick={signin}>
+										sign in
+									</Button>
+									<Button className={classes.containedButtonStyle} variant='text' onClick={signup}>
+										get started
+									</Button>
+								</>
+							)}
+						</Box>
+					</Grid>
+				</Grid>
+			</AppBar>
+		</Container>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-   appBarStyle: {
-      backgroundColor: '#F2F2F2',
-      borderBottom: '1px solid #730217',
-      boxShadow: 'none',
-      [theme.breakpoints.down('sm')]: {
-         position: 'relative',
-         borderBottom: 'none',
-      },
-   },
-   gridStyle: {
-      justifyContent: 'flex-start',
-      display: 'flex',
-      flexDirection: 'row',
-      [theme.breakpoints.down('sm')]: {
-         justifyContent: 'center',
-      },
-   },
-   avatarStyle: {
-      width: '250px',
-      height: 'auto',
-      borderRadius: 0,
-      margin: '10px',
-      [theme.breakpoints.down('sm')]: {},
-   },
-   menuStyle: {
-      textAlign: 'right',
-      [theme.breakpoints.down('sm')]: {
-         textAlign: 'center',
-         padding: '10px 0',
-      },
-   },
-   boxStyle: {
-      display: 'inline',
-      borderLeft: '1px solid #730217',
-      padding: '15px 0 15px 15px',
-      marginLeft: '15px',
-      [theme.breakpoints.down('xs')]: {
-         display: 'block',
-         borderLeft: 'none',
-         marginLeft: '0',
-      },
-   },
-   textButtonStyle: {
-      fontSize: '15px',
-      fontWeight: '400',
-      backgroundColor: 'transparent',
-      color: '#730217',
-      padding: '10px',
-      margin: '0 5px',
-      textTransform: 'lowercase',
-      '&:hover': {
-         backgroundColor: 'transparent',
-         color: '#BF0426',
-      },
-      [theme.breakpoints.down('sm')]: {
-         fontSize: '12px',
-      },
-   },
-   containedButtonStyle: {
-      fontSize: '15px',
-      fontWeight: '400',
-      textTransform: 'lowercase',
-      backgroundColor: '#730217',
-      color: '#F2F2F2',
-      padding: '10px 20px',
-      boxShadow: 'none',
-      '&:hover': {
-         backgroundColor: '#BF0426',
-         boxShadow: 'none',
-      },
-      [theme.breakpoints.down('sm')]: {
-         fontSize: '12px',
-      },
-   },
+	appBarStyle: {
+		backgroundColor: '#F2F2F2',
+		borderBottom: '1px solid #730217',
+		boxShadow: 'none',
+		[theme.breakpoints.down('sm')]: {
+			position: 'relative',
+			borderBottom: 'none',
+		},
+	},
+	gridStyle: {
+		justifyContent: 'flex-start',
+		display: 'flex',
+		flexDirection: 'row',
+		[theme.breakpoints.down('sm')]: {
+			justifyContent: 'center',
+		},
+	},
+	avatarStyle: {
+		width: '250px',
+		height: 'auto',
+		borderRadius: 0,
+		margin: '10px',
+		[theme.breakpoints.down('sm')]: {},
+	},
+	menuStyle: {
+		textAlign: 'right',
+		[theme.breakpoints.down('sm')]: {
+			textAlign: 'center',
+			padding: '10px 0',
+		},
+	},
+	boxStyle: {
+		display: 'inline',
+		borderLeft: '1px solid #730217',
+		padding: '15px 0 15px 15px',
+		marginLeft: '15px',
+		[theme.breakpoints.down('xs')]: {
+			display: 'block',
+			borderLeft: 'none',
+			marginLeft: '0',
+		},
+	},
+	textButtonStyle: {
+		fontSize: '15px',
+		fontWeight: '400',
+		backgroundColor: 'transparent',
+		color: '#730217',
+		padding: '10px',
+		margin: '0 5px',
+		textTransform: 'lowercase',
+		'&:hover': {
+			backgroundColor: 'transparent',
+			color: '#BF0426',
+		},
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '12px',
+		},
+	},
+	containedButtonStyle: {
+		fontSize: '15px',
+		fontWeight: '400',
+		textTransform: 'lowercase',
+		backgroundColor: '#730217',
+		color: '#F2F2F2',
+		padding: '10px 20px',
+		boxShadow: 'none',
+		'&:hover': {
+			backgroundColor: '#BF0426',
+			boxShadow: 'none',
+		},
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '12px',
+		},
+	},
 }));
 
 export default Navbar;
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,15 +1,5 @@
 import React, { useState, useContext } from 'react';
-import {
-   Avatar,
-   Button,
-   CssBaseline,
-   TextField,
-   Link,
-   Grid,
-   Box,
-   Typography,
-   Container,
-} from '@material-ui/core';
+import { Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from './logo-vertical.png';
 import { useHistory } from 'react-router-dom';
@@ -19,226 +9,199 @@ import ErrorNotice from '../../components/ErrorNotice';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 function SignUp() {
-   const [email, setEmail] = useState();
-   const [password, setPassword] = useState();
-   const [name, setName] = useState();
-   const [error, setError] = useState();
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const [name, setName] = useState();
+	const [error, setError] = useState();
 
-   const { setUserData } = useContext(UserContext);
+	const { setUserData } = useContext(UserContext);
 
-   const history = useHistory();
+	const history = useHistory();
 
-   const submit = async (e) => {
-      e.preventDefault();
-      try {
-         const newUser = { name, email, password };
-         await axios.post('http://localhost:5000/api/users', newUser);
-         const loginRes = await axios.post('http://localhost:5000/api/auth', {
-            email,
-            password,
-         });
-         setUserData({
-            token: loginRes.data.token,
-            name: loginRes.data.name,
-         });
-         localStorage.setItem('auth-token', loginRes.data.token);
-         history.push('/home');
-      } catch (err) {
-         err.response.data.msg && setError(err.response.data.msg);
-      }
-   };
+	const submit = async (e) => {
+		e.preventDefault();
+		try {
+			const newUser = { name, email, password };
+			await axios.post('http://localhost:5000/api/users', newUser);
+			const loginRes = await axios.post('http://localhost:5000/api/auth', {
+				email,
+				password,
+			});
+			setUserData({
+				token: loginRes.data.token,
+				name: loginRes.data.name,
+			});
+			localStorage.setItem('auth-token', loginRes.data.token);
+			history.push('/home');
+		} catch (err) {
+			err.response.data.msg && setError(err.response.data.msg);
+		}
+	};
 
-   const classes = useStyles();
+	const classes = useStyles();
 
-   const welcome = () => history.push('/');
-   const signin = () => history.push('/signin');
+	const welcome = () => history.push('/');
+	const signin = () => history.push('/signin');
 
-   return (
-      <Container maxWidth={false} className={classes.containerStyle}>
-         <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-               <Button className={classes.buttonLogoStyle} onClick={welcome}>
-                  <Avatar
-                     className={classes.avatarStyle}
-                     src={logo}
-                     justify="center"
-                  ></Avatar>
-               </Button>
-               <Typography
-                  component="h1"
-                  variant="h5"
-                  style={{ color: '#730217' }}
-               >
-                  Sign up
-               </Typography>
-               <form className={classes.form} noValidate>
-                  <Grid container spacing={2}>
-                     <Grid item xs={12}>
-                        <TextField
-                           autoComplete="uname"
-                           name="userName"
-                           variant="outlined"
-                           required
-                           fullWidth
-                           id="userName"
-                           label="Username"
-                           autoFocus
-                           onChange={(e) => setName(e.target.value)}
-                           color="secondary"
-                        />
-                     </Grid>
-                     <Grid item xs={12}>
-                        <TextField
-                           variant="outlined"
-                           required
-                           fullWidth
-                           id="email"
-                           label="Email Address"
-                           name="email"
-                           autoComplete="email"
-                           onChange={(e) => setEmail(e.target.value)}
-                           color="secondary"
-                        />
-                     </Grid>
-                     <Grid item xs={12}>
-                        <TextField
-                           variant="outlined"
-                           required
-                           fullWidth
-                           name="password"
-                           label="Password"
-                           type="password"
-                           id="password"
-                           autoComplete="current-password"
-                           onChange={(e) => setPassword(e.target.value)}
-                           color="secondary"
-                        />
-                     </Grid>
-                  </Grid>
-                  <Button
-                     type="submit"
-                     fullWidth
-                     variant="contained"
-                     color="primary"
-                     className={classes.containedButtonStyle}
-                     onClick={submit}
-                  >
-                     Sign Up
-                  </Button>
-                  {error && (
-                     <ErrorNotice
-                        message={error}
-                        clearError={() => setError(undefined)}
-                     />
-                  )}
-                  <Grid container justify="flex-end">
-                     <Grid item xs>
-                        <Link
-                           href="/signin"
-                           variant="body2"
-                           style={{ color: '#730217' }}
-                           onClick={signin}
-                        >
-                           Already have an account? Sign in
-                        </Link>
-                     </Grid>
-                  </Grid>
-               </form>
-            </div>
-            <Grid
-               style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-               }}
-            >
-               <Box mt={5}>
-                  <Typography
-                     className={classes.madeWithLove}
-                     color="textSecondary"
-                  >
-                     Made with{' '}
-                     <FavoriteIcon
-                        style={{
-                           color: 'red',
-                           padding: '0 8px',
-                           fontSize: '35px',
-                        }}
-                     />
-                     by SMU Bootcamp Students
-                  </Typography>
-                  <Copyright />
-               </Box>
-            </Grid>
-         </Container>
-      </Container>
-   );
+	return (
+		<Container maxWidth={false} className={classes.containerStyle}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Button className={classes.buttonLogoStyle} onClick={welcome}>
+						<Avatar className={classes.avatarStyle} src={logo} justify='center'></Avatar>
+					</Button>
+					<Typography component='h1' variant='h5' style={{ color: '#730217' }}>
+						Sign up
+					</Typography>
+					<form className={classes.form} noValidate>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									autoComplete='uname'
+									name='userName'
+									variant='outlined'
+									required
+									fullWidth
+									id='userName'
+									label='Username'
+									autoFocus
+									onChange={(e) => setName(e.target.value)}
+									color='secondary'
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant='outlined'
+									required
+									fullWidth
+									id='email'
+									label='Email Address'
+									name='email'
+									autoComplete='email'
+									onChange={(e) => setEmail(e.target.value)}
+									color='secondary'
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant='outlined'
+									required
+									fullWidth
+									name='password'
+									label='Password'
+									type='password'
+									id='password'
+									autoComplete='current-password'
+									onChange={(e) => setPassword(e.target.value)}
+									color='secondary'
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							color='primary'
+							className={classes.containedButtonStyle}
+							onClick={submit}>
+							Sign Up
+						</Button>
+						{error && <ErrorNotice message={error} clearError={() => setError(undefined)} />}
+						<Grid container justify='flex-end'>
+							<Grid item xs>
+								<Link href='/signin' variant='body2' style={{ color: '#730217' }} onClick={signin}>
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+				<Grid
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}>
+					<Box mt={5}>
+						<Typography className={classes.madeWithLove} color='textSecondary'>
+							Made with{' '}
+							<FavoriteIcon
+								style={{
+									color: 'red',
+									padding: '0 8px',
+									fontSize: '35px',
+								}}
+							/>
+							by SMU Bootcamp Students
+						</Typography>
+						<Copyright />
+					</Box>
+				</Grid>
+			</Container>
+		</Container>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-   paper: {
-      marginTop: theme.spacing(2),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-   },
-   containerStyle: {
-      backgroundColor: '#F2F2F2',
-   },
-   avatarStyle: {
-      backgroundColor: '#F2F2F2',
-      width: '120px',
-      height: 'auto',
-      margin: '0 auto 20px',
-      padding: '20px',
-      borderRadius: '5px',
-   },
-   form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-   },
-   containedButtonStyle: {
-      fontSize: '20px',
-      fontWeight: '400',
-      textTransform: 'lowercase',
-      backgroundColor: '#730217',
-      color: '#F2F2F2',
-      padding: '10px 20px',
-      margin: '10px 0',
-      boxShadow: 'none',
-      '&:hover': {
-         backgroundColor: '#BF0426',
-         boxShadow: 'none',
-      },
-   },
-   madeWithLove: {
-      fontSize: '12px',
-      letterSpacing: '1px',
-      display: 'flex',
-      alignItems: 'center',
-      paddingBottom: '10px',
-      [theme.breakpoints.down('xs')]: {
-         fontSize: '12px',
-         letterSpacing: '1px',
-      },
-   },
+	paper: {
+		marginTop: theme.spacing(2),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	containerStyle: {
+		backgroundColor: '#F2F2F2',
+	},
+	avatarStyle: {
+		backgroundColor: '#F2F2F2',
+		width: '120px',
+		height: 'auto',
+		margin: '0 auto 20px',
+		padding: '20px',
+		borderRadius: '5px',
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(3),
+	},
+	containedButtonStyle: {
+		fontSize: '20px',
+		fontWeight: '400',
+		textTransform: 'lowercase',
+		backgroundColor: '#730217',
+		color: '#F2F2F2',
+		padding: '10px 20px',
+		margin: '10px 0',
+		boxShadow: 'none',
+		'&:hover': {
+			backgroundColor: '#BF0426',
+			boxShadow: 'none',
+		},
+	},
+	madeWithLove: {
+		fontSize: '12px',
+		letterSpacing: '1px',
+		display: 'flex',
+		alignItems: 'center',
+		paddingBottom: '10px',
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '12px',
+			letterSpacing: '1px',
+		},
+	},
 }));
 
 function Copyright() {
-   return (
-      <Typography
-         style={{ fontSize: '12px', letterSpacing: '1px' }}
-         color="textSecondary"
-         align="center"
-      >
-         {'Copyright © '}
-         <Link color="inherit" href="https://material-ui.com/">
-            Chief System
-         </Link>{' '}
-         {new Date().getFullYear()}
-         {'.'}
-      </Typography>
-   );
+	return (
+		<Typography style={{ fontSize: '12px', letterSpacing: '1px' }} color='textSecondary' align='center'>
+			{'Copyright © '}
+			<Link color='inherit' href='https://lit-meadow-59526.herokuapp.com'>
+				Chief System
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 export default SignUp;
